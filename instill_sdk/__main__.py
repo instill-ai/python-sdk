bc25c89cfe2a541f1dedec3690dbb38496d85bd9
Heiru Wu <heiru.wu@instill.tech>
2023-09-19 16:29:55 +0800

    feat(config): adopt pydantic for yaml config validation

--- instill_sdk/__main__.py
+++ instill_sdk/__main__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 """Package entry point."""
+import pprint
 
+from instill_sdk.configuration import config
 
 # from instill_sdk.client import
 
 if __name__ == "__main__":  # pragma: no cover
     # main()  # pylint: disable=no-value-for-parameter
-    print("hello world")
+    print("======================Configured Hosts======================")
+    pprint.pprint(config.remotes)
