96df988c8aae8cb9034a0cf11cb8013a81c3c5f5
HR Wu <5631010+heiruwu@users.noreply.github.com>
2023-09-26 00:59:42 +0800

    test(integration): add instill model connector (#21)
    Because
    
    - add `instill model` connector test
    
    This commit
    
    - add `instill model` connector test
    - bump protogen-python

--- janky-integration-test.py
+++ janky-integration-test.py
@@ -1,4 +1,7 @@
 # pylint: disable=no-member,no-name-in-module
+import requests
+import base64
+
 from google.protobuf.struct_pb2 import Struct
 from instill.clients import get_client
 from instill.resources.model import GithubModel
@@ -18,8 +21,8 @@ local_model = {
     "model_path": "/Users/heiru/Projects/instill/model-backend/integration-test/data/dummy-cls-model.zip",
 }
 github_model = {
-    "model_name": "mobilenetv2",
-    "model_repo": "instill-ai/model-mobilenetv2-dvc",
+    "model_name": "yolov7",
+    "model_repo": "instill-ai/model-yolov7-dvc",
     "model_tag": "v1.0-cpu",
 }
 
@@ -71,35 +74,37 @@ try:
     ]
 
     outputs = model(task_inputs=task_inputs)
-    assert outputs[0].classification.category == "golden retriever"
-    Logger.i("inference done, assert output 0 category == golden retriever: True")
-    assert outputs[1].classification.category == "brown bear"
-    Logger.i("inference done, assert output 1 category == brown bear: True")
-    assert outputs[2].classification.category == "ice bear"
-    Logger.i("inference done, assert output 2 category == ice bear: True")
+    assert outputs[0].detection.objects[0].category == "dog"
+    Logger.i("inference done, assert output 0 object 0 category == dog: True")
+    assert outputs[0].detection.objects[1].category == "dog"
+    Logger.i("inference done, assert output 0 object 1 category == dog: True")
+    assert outputs[1].detection.objects[0].category == "bear"
+    Logger.i("inference done, assert output 1 category == bear: True")
+    assert outputs[2].detection.objects[0].category == "bear"
+    Logger.i("inference done, assert output 2 category == bear: True")
 
     # ======================== connector
     assert client.connector_service.is_serving()
     Logger.i("connector client created, assert status == serving: True")
 
-    mobilenet_connector = InstillModelConnector(
+    instill_connector = InstillModelConnector(
         client,
-        "mobilenetv2",
-        "http://model-backend:8083",
+        name="instill",
+        server_url="http://api-gateway:8080",
     )
     assert (
-        mobilenet_connector.get_state()
+        instill_connector.get_state()
         == connector_interface.ConnectorResource.STATE_DISCONNECTED
     )
     Logger.i(
         "instill model connector created, assert state == STATE_DISCONNECTED: True"
     )
 
-    # assert (
-    #     mobilenet_connector.test()
-    #     == connector_interface.ConnectorResource.STATE_CONNECTED
-    # )
-    # Logger.i("instill model connector, assert state == STATE_CONNECTED: True")
+    assert (
+        instill_connector.test()
+        == connector_interface.ConnectorResource.STATE_CONNECTED
+    )
+    Logger.i("instill model connector, assert state == STATE_CONNECTED: True")
 
     config = {"destination_path": "/local/test-1"}
     csv_connector = Connector(
@@ -117,7 +122,7 @@ try:
     assert csv_connector.test() == connector_interface.ConnectorResource.STATE_CONNECTED
     Logger.i("tested csv connector, assert state == STATE_CONNECTED: True")
 
-    # ======================== pipeline
+    # ======================== csv pipeline
     assert client.pipeline_service.is_serving()
     Logger.i("pipeline client created, assert status == serving: True")
 
@@ -150,18 +155,76 @@ try:
     recipe.components.append(end_operator_component)
     recipe.components.append(csv_connector_component)
 
-    pipeline = Pipeline(client=client, name="csv-pipeline", recipe=recipe)
-    pipeline.validate_pipeline()
-    Logger.i("pipeline csv-pipeline created, validate without error: True")
+    csv_pipeline = Pipeline(client=client, name="csv-pipeline", recipe=recipe)
+    csv_pipeline.validate_pipeline()
+    Logger.i("csv-pipeline created, validate without error: True")
     i = Struct()
     i.update({"input": "instill-ai rocks"})
-    assert pipeline([i])[0][0]["answer"]["text"] == "instill-ai rocks"
-    Logger.i("pipeline csv-pipeline triggered, output matched input: True")
+    assert csv_pipeline([i])[0][0]["answer"]["text"] == "instill-ai rocks"
+    Logger.i("csv-pipeline triggered, output matched input: True")
+
+    # ======================== instill model pipeline
+    start_operator_component = pipeline_interface.Component()
+    start_operator_component.id = "start"
+    start_operator_component.definition_name = "operator-definitions/start-operator"
+    start_operator_component.configuration.update(
+        {"metadata": {"input": {"title": "input", "type": "image"}}}
+    )
+    instill_model_connector_component = pipeline_interface.Component()
+    instill_model_connector_component.id = "yolov7"
+    instill_model_connector_component.resource_name = (
+        f"{user.name}/connector-resources/instill"
+    )
+    instill_model_connector_component.definition_name = (
+        instill_connector.get_definition().name
+    )
+    instill_model_connector_component.configuration.update(
+        {
+            "input": {
+                "task": "TASK_DETECTION",
+                "image_base64": "{ start.input }",
+                "model_name": "users/admin/models/yolov7",
+            },
+        }
+    )
+
+    end_operator_component = pipeline_interface.Component()
+    end_operator_component.id = "end"
+    end_operator_component.definition_name = "operator-definitions/end-operator"
+    end_operator_component.configuration.update(
+        {"input": {"output": "{ yolov7.output.objects }"}, "metadata": {"output": {}}}
+    )
+
+    recipe = pipeline_interface.Recipe()
+    recipe.version = "v1alpha"
+    recipe.components.append(start_operator_component)
+    recipe.components.append(instill_model_connector_component)
+    recipe.components.append(end_operator_component)
+    instill_model_pipeline = Pipeline(
+        client=client, name="instill-model-pipeline", recipe=recipe
+    )
+    instill_model_pipeline.validate_pipeline()
+    Logger.i("instill-model-pipeline created, validate without error: True")
+    i = Struct()
+    i.update(
+        {
+            "input": base64.b64encode(
+                requests.get(
+                    "https://artifacts.instill.tech/imgs/dog.jpg", timeout=5
+                ).content
+            ).decode("ascii")
+        }
+    )
+
+    assert instill_model_pipeline([i])[0][0]["output"][0]["category"] == "dog"
+    Logger.i("instill-model-pipeline triggered, output matched input: True")
 except AssertionError:
     Logger.w("TEST FAILED, ASSERTION MISMATCHED")
 
 Logger.i("====================Test Done====================")
 Logger.i("===================Cleaning up===================")
-del pipeline
+del csv_pipeline
+del instill_model_pipeline
 del model
+del instill_connector
 del csv_connector
