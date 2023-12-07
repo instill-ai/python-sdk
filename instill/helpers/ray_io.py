4e09a729fae2b9e45fd54309bc56b8b4f6d8723f
HR Wu <5631010+heiruwu@users.noreply.github.com>
2023-12-08 07:20:00 +0800

    chore(notebooks): add text-to-image example (#62)
    Because
    
    - provide more example for serving custom models
    
    This commit
    
    - add text-to-image example

--- instill/helpers/ray_io.py
+++ instill/helpers/ray_io.py
@@ -1,6 +1,7 @@
 import io
 import json
 import struct
+from json.decoder import JSONDecodeError
 from typing import List
 
 import numpy as np
@@ -363,9 +364,14 @@ class StandardTaskIO:
 
             if input_name == "conversation":
                 input_tensor = deserialize_bytes_tensor(b_input_tensor)
-                text_generation_chat_input.conversation = str(
-                    input_tensor[0].decode("utf-8")
-                )
+                try:
+                    text_generation_chat_input.conversation = json.loads(
+                        str(input_tensor[0].decode("utf-8"))
+                    )
+                except Exception as e:
+                    raise JSONDecodeError(
+                        "can't parse conversation json string", "", 0
+                    ) from e
                 print(
                     f"[DEBUG] input `conversation` type\
                         ({type(text_generation_chat_input.conversation)}): {text_generation_chat_input.conversation}"
