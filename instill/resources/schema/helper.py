25576aae7c09d7c60a64e6427842927db903f5b1
Heiru Wu <heiru.wu@instill.tech>
2024-01-09 01:52:16 +0800

    chore: update helper for populating default val

--- instill/resources/schema/helper.py
+++ instill/resources/schema/helper.py
@@ -1,14 +1,22 @@
+import re
+
+
 def populate_default_value(dataclass):
     for field in dataclass.__dataclass_fields__.values():
-        if field.default is None and isinstance(field.type, bool):
-            setattr(dataclass, field.name, False)
-        if field.default is None and isinstance(field.type, str):
-            setattr(dataclass, field.name, "")
-        if field.default is None and isinstance(field.type, int):
-            setattr(dataclass, field.name, 0)
-        if field.default is None and isinstance(field.type, float):
-            setattr(dataclass, field.name, 0.0)
-        if field.default is None and isinstance(field.type, dict):
-            setattr(dataclass, field.name, {})
-        if field.default is None and isinstance(field.type, list):
-            setattr(dataclass, field.name, [])
+        if field.default is None:
+            list_pattern = re.compile(r"^Optional\[List")
+            other_optional_pattern = re.compile(r"^Optional\[")
+            if field.type == "Optional[bool]":
+                setattr(dataclass, field.name, False)
+            elif field.type == "Optional[str]":
+                setattr(dataclass, field.name, "")
+            elif field.type == "Optional[int]":
+                setattr(dataclass, field.name, 0)
+            elif field.type == "Optional[float]":
+                setattr(dataclass, field.name, 0.0)
+            elif list_pattern.match(field.type):
+                setattr(dataclass, field.name, [])
+            elif other_optional_pattern.match(field.type):
+                setattr(dataclass, field.name, {})
+
+    return dataclass
