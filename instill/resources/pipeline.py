89431fa39f626736b16ee279ae4b6c9f19a3c1c3
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-01-11 04:50:06 +0800

    feat(resources): support recipe update in pipeline resource (#83)
    Because
    
    - currently we dont support updating recipe in pipeline resource object
    
    This commit
    
    - support recipe update in pipeline resource

--- instill/resources/pipeline.py
+++ instill/resources/pipeline.py
@@ -3,6 +3,7 @@ from typing import Tuple, Union
 
 import grpc
 from google.longrunning import operations_pb2
+from google.protobuf.field_mask_pb2 import FieldMask
 
 import instill.protogen.vdp.pipeline.v1beta.pipeline_pb2 as pipeline_interface
 from instill.clients import InstillClient
@@ -66,8 +67,16 @@ class Pipeline(Resource):
             self.resource.id, task_inputs
         ).operation
 
-    def get_recipe(self) -> str:
+    def get_recipe(self) -> pipeline_interface.Recipe:
         return self.resource.recipe
+
+    def update_recipe(self, recipe: pipeline_interface.Recipe):
+        pipeline = self.resource
+        pipeline.recipe.CopyFrom(recipe)
+        self.client.pipeline_service.update_pipeline(
+            pipeline, FieldMask(paths=["recipe"])
+        )
+        self._update()
 
     def validate_pipeline(self) -> bool:
         try:
