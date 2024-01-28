82adefaace490dad991c83c03264d448bd0e25a5
Heiru Wu <heiru.wu@instill.tech>
2024-01-29 07:53:06 +0800

    fix(ray): update autoscaling configs

--- instill/helpers/const.py
+++ instill/helpers/const.py
@@ -88,12 +88,12 @@ DEFAULT_AUTOSCALING_CONFIG = {
     "target_num_ongoing_requests_per_replica": 1,
     "initial_replicas": 1,
     "min_replicas": 0,
-    "max_replicas": 20,
+    "max_replicas": 10,
     "upscale_delay_s": 4,
-    "downscale_delay_s": 1800,
+    "downscale_delay_s": 600,
     "smoothing_factor": 1.0,
-    "upscale_smoothing_factor": 1.5,
-    "downscale_smoothing_factor": 0.3,
+    "upscale_smoothing_factor": 0.8,
+    "downscale_smoothing_factor": 0.8,
     "metrics_interval_s": 2,
     "look_back_period_s": 4,
 }
@@ -102,7 +102,7 @@ DEFAULT_RUNTIME_ENV = {
         "PYTHONPATH": os.getcwd(),
     },
 }
-DEFAULT_MAX_CONCURRENT_QUERIES = 10
+DEFAULT_MAX_CONCURRENT_QUERIES = 5
 
 RAM_MINIMUM_RESERVE = 1  # GB
 RAM_UPSCALE_FACTOR = 1.25
