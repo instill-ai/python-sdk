360dbdd9c9b8c770aa1af620a10926b548c56bc5
Heiru Wu <heiru.wu@instill.tech>
2024-02-15 23:27:32 +0800

    chore(ray): update override model name

--- instill/helpers/const.py
+++ instill/helpers/const.py
@@ -112,7 +112,7 @@ VRAM_UPSCALE_FACTOR = 1.25
 MODEL_VRAM_OVERRIDE_LIST = {
     "stable-diffusion-xl": 0.45,
     "controlnet-canny": 0.45,
-    "llava-7b": 0.4,
+    "llava-1-6-7b": 0.4,
     "llama2-7b-chat": 0.4,
     "llama2-7b": 0.4,
     "zephyr-7b": 0.4,
