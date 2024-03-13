b2775f593c85c20ca38106bf0f9447120825bc3d
Heiru Wu <heiru.wu@instill.tech>
2024-03-13 22:09:42 +0800

    feat(ray): add llava 13b to predeploy list

--- instill/helpers/const.py
+++ instill/helpers/const.py
@@ -110,10 +110,11 @@ VRAM_MINIMUM_RESERVE = 2  # GB
 VRAM_UPSCALE_FACTOR = 1.25
 
 MODEL_VRAM_OVERRIDE_LIST = {
-    "stable-diffusion-xl": 0.45,
-    "controlnet-canny": 0.45,
-    "llava-1-6-7b": 0.4,
-    "llama2-7b-chat": 0.4,
+    "stable-diffusion-xl": 0.4,
+    "controlnet-canny": 0.2,
+    "llava-1-6-7b": 0.2,
+    "llava-1-6-13b": 0.4,
+    "llama2-7b-chat": 0.2,
     "llama2-7b": 0.4,
     "zephyr-7b": 0.4,
 }
