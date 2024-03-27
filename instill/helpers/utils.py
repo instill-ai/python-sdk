dac75a934d9c42f16a5089aa55fad4b11fc90d2a
Heiru Wu <heiru.wu@instill.tech>
2024-03-27 17:01:41 +0800

    fix(ray): fix etrypoint module not found

--- instill/helpers/utils.py
+++ instill/helpers/utils.py
@@ -1,12 +1,32 @@
 import os
 
+IGNORE_FILES = {
+    ".bash_logout",
+    ".bashrc",
+    ".profile",
+    ".sudo_as_admin_successful",
+    "pip-freeze.txt",
+    "requirements_compiled.txt",
+}
+
+IGNORE_FOLDERS = {
+    ".cache",
+    ".conda",
+    ".whl",
+    "anaconda3",
+}
+
 
 def get_dir_size(path):
     total = 0
     with os.scandir(path) as it:
         for entry in it:
             if entry.is_file():
+                if entry.name in IGNORE_FILES:
+                    continue
                 total += entry.stat().st_size
             elif entry.is_dir():
+                if entry.name in IGNORE_FOLDERS:
+                    continue
                 total += get_dir_size(entry.path)
     return total
