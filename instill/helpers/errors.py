95cc2e2434cc04f38155b15ee7349f8dc37ab3cb
Heiru Wu <heiru.wu@instill.tech>
2024-04-30 18:34:53 +0800

    feat(ray): implement standalone executable and optional system packages

--- instill/helpers/errors.py
+++ instill/helpers/errors.py
@@ -6,3 +6,13 @@ class ModelPathException(Exception):
 class ModelVramException(Exception):
     def __str__(self) -> str:
         return "model projected vram usage is more than the GPU can handle"
+
+
+class ModelConfigException(Exception):
+    def __init__(self, field):
+        self.field = field
+
+    def __str__(
+        self,
+    ) -> str:
+        return f"model config file `instill.yaml` is missing {self.field} field"
