a299c9846c5adcce75ad455a7457ac44fe74f52d
Heiru Wu <heiru.wu@instill.tech>
2024-05-16 01:51:52 +0800

    chore(deps): update upstream deprecated configs

--- instill/helpers/const.py
+++ instill/helpers/const.py
@@ -85,15 +85,15 @@ DEFAULT_RAY_ACTOR_OPTIONS = {
     "num_cpus": 0,
 }
 DEFAULT_AUTOSCALING_CONFIG = {
-    "target_num_ongoing_requests_per_replica": 2,
+    "target_ongoing_requests": 2,
     "initial_replicas": 1,
     "min_replicas": 0,
-    "max_replicas": 10,
+    "max_replicas": 1,
     "upscale_delay_s": 180,
-    "downscale_delay_s": 120,
+    "downscale_delay_s": 360,
     "smoothing_factor": 1.0,
-    "upscale_smoothing_factor": 0.8,
-    "downscale_smoothing_factor": 0.8,
+    "upscaling_factor": 0.8,
+    "downscaling_factor": 0.8,
     "metrics_interval_s": 2,
     "look_back_period_s": 4,
 }
@@ -102,7 +102,8 @@ DEFAULT_RUNTIME_ENV = {
         "PYTHONPATH": os.getcwd(),
     },
 }
-DEFAULT_MAX_CONCURRENT_QUERIES = 5
+DEFAULT_MAX_ONGOING_REQUESTS = 5
+DEFAULT_MAX_QUEUED_REQUESTS = 10
 
 RAM_MINIMUM_RESERVE = 1  # GB
 RAM_UPSCALE_FACTOR = 1.25
