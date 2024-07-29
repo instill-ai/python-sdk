cab423c65f0ee9d4e29a8bc1e2d57d7cca5c10d0
Jeremy Shih <jeremyshih0628@gmail.com>
2024-07-29 17:14:02 +0800

    feat(artifact): updated proto and added client code

--- instill/clients/client.py
+++ instill/clients/client.py
@@ -1,4 +1,5 @@
 # pylint: disable=no-name-in-module
+from instill.clients.artifact import ArtifactClient
 from instill.clients.mgmt import MgmtClient
 from instill.clients.model import ModelClient
 from instill.clients.pipeline import PipelineClient
@@ -26,11 +27,19 @@ class InstillClient:
             Logger.w(
                 "Instill Model is not serving, Model functionalities will not work"
             )
+        self.artifact_service = ArtifactClient(
+            async_enabled=async_enabled,
+        )
+        if not self.model_service.is_serving():
+            Logger.w(
+                "Instill Artifact is not serving, Artifact functionalities will not work"
+            )
 
     def set_instance(self, instance: str):
         self.mgmt_service.instance = instance
         self.pipeline_service.instance = instance
         self.model_service.instance = instance
+        self.artifact_service.instance = instance
 
     def close(self):
         if self.mgmt_service.is_serving():
@@ -42,6 +51,9 @@ class InstillClient:
         if self.model_service.is_serving():
             for host in self.model_service.hosts.values():
                 host.channel.close()
+        if self.artifact_service.is_serving():
+            for host in self.artifact_service.hosts.values():
+                host.channel.close()
 
     async def async_close(self):
         if self.mgmt_service.is_serving():
@@ -52,6 +64,9 @@ class InstillClient:
                 await host.async_channel.close()
         if self.model_service.is_serving():
             for host in self.model_service.hosts.values():
+                await host.async_channel.close()
+        if self.artifact_service.is_serving():
+            for host in self.artifact_service.hosts.values():
                 await host.async_channel.close()
 
 
