d7be871c1fd8b9a42c31956742f4bddf4dc5c049
Heiru Wu <heiru.wu@instill.tech>
2024-08-08 15:50:31 +0800

    chore: add completion task

--- instill/helpers/__init__.py
+++ instill/helpers/__init__.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-name-in-module
 from instill.helpers.protobufs.ray_pb2 import CallRequest, CallResponse
 from instill.helpers.ray_io import (
+    construct_task_completion_output,
     construct_task_chat_output,
     construct_task_classification_output,
     construct_task_detection_output,
@@ -8,6 +9,7 @@ from instill.helpers.ray_io import (
     construct_task_keypoint_output,
     construct_task_ocr_output,
     construct_task_semantic_segmentation_output,
+    parse_task_completion_to_completion_input,
     parse_task_chat_to_chat_input,
     parse_task_chat_to_multimodal_chat_input,
     parse_task_classification_to_vision_input,
