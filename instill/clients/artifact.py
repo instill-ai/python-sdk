7b2aaa154f09d78df2332d18aceff6b656345193
Jeremy Shih <jeremyshih0628@gmail.com>
2024-08-27 16:22:08 +0800

    feat(client): update client code for artifact

--- instill/clients/artifact.py
+++ instill/clients/artifact.py
@@ -5,6 +5,9 @@ from typing import Dict
 import instill.protogen.artifact.artifact.v1alpha.artifact_pb2 as artifact_interface
 import instill.protogen.artifact.artifact.v1alpha.artifact_public_service_pb2_grpc as artifact_service
 import instill.protogen.artifact.artifact.v1alpha.chunk_pb2 as chunk_interface
+import instill.protogen.artifact.artifact.v1alpha.conversation_pb2 as conversation_interface
+import instill.protogen.artifact.artifact.v1alpha.file_catalog_pb2 as file_catalog_interface
+import instill.protogen.artifact.artifact.v1alpha.qa_pb2 as qa_interface
 
 # common
 import instill.protogen.common.healthcheck.v1beta.healthcheck_pb2 as healthcheck
@@ -415,7 +418,7 @@ class ArtifactClient(Client):
         namespace_id: str,
         catalog_id: str,
         text_prompt: str,
-        topk: int,
+        top_k: int,
         async_enabled: bool = False,
     ) -> chunk_interface.SimilarityChunksSearchResponse:
         if async_enabled:
@@ -425,7 +428,7 @@ class ArtifactClient(Client):
                     namespace_id=namespace_id,
                     catalog_id=catalog_id,
                     text_prompt=text_prompt,
-                    topk=topk,
+                    top_k=top_k,
                 ),
                 metadata=self.hosts[self.instance].metadata,
             ).send_async()
@@ -436,7 +439,338 @@ class ArtifactClient(Client):
                 namespace_id=namespace_id,
                 catalog_id=catalog_id,
                 text_prompt=text_prompt,
-                topk=topk,
+                top_k=top_k,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def question_answering(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        question: str,
+        top_k: int,
+        async_enabled: bool = False,
+    ) -> qa_interface.QuestionAnsweringResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.QuestionAnswering,
+                request=qa_interface.QuestionAnsweringRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    question=question,
+                    top_k=top_k,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.QuestionAnswering,
+            request=qa_interface.QuestionAnsweringRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                question=question,
+                top_k=top_k,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def get_file_catalog(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        file_id: str,
+        file_uid: str,
+        async_enabled: bool = False,
+    ) -> file_catalog_interface.GetFileCatalogResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.GetFileCatalog,
+                request=file_catalog_interface.GetFileCatalogRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    file_id=file_id,
+                    file_uid=file_uid,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.GetFileCatalog,
+            request=file_catalog_interface.GetFileCatalogRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                file_id=file_id,
+                file_uid=file_uid,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def create_conversation(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.CreateConversationResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.CreateConversation,
+                request=conversation_interface.CreateConversationRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.CreateConversation,
+            request=conversation_interface.CreateConversationRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def list_conversations(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        page_size: int = 100,
+        page_token: str = "",
+        async_enabled: bool = False,
+    ) -> conversation_interface.ListConversationsResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.ListConversations,
+                request=conversation_interface.ListConversationsRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    page_size=page_size,
+                    page_token=page_token,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.ListConversations,
+            request=conversation_interface.ListConversationsRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                page_size=page_size,
+                page_token=page_token,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def update_conversation(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        new_conversation_id: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.UpdateConversationResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.UpdateConversation,
+                request=conversation_interface.UpdateConversationRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                    new_conversation_id=new_conversation_id,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.UpdateConversation,
+            request=conversation_interface.UpdateConversationRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+                new_conversation_id=new_conversation_id,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def delete_conversation(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.DeleteConversationResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.DeleteConversation,
+                request=conversation_interface.DeleteConversationRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.DeleteConversation,
+            request=conversation_interface.DeleteConversationRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def create_message(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        content: str,
+        role: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.CreateMessageResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.CreateMessage,
+                request=conversation_interface.CreateMessageRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                    content=content,
+                    role=role,
+                    type=conversation_interface.Message.MessageType.MESSAGE_TYPE_TEXT,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.CreateMessage,
+            request=conversation_interface.CreateMessageRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+                content=content,
+                role=role,
+                type=conversation_interface.Message.MessageType.MESSAGE_TYPE_TEXT,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def list_messages(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        latest_k: int,
+        include_system_messages: bool,
+        page_size: int = 100,
+        page_token: str = "",
+        async_enabled: bool = False,
+    ) -> conversation_interface.ListMessagesResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.ListMessages,
+                request=conversation_interface.ListMessagesRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                    latest_k=latest_k,
+                    include_system_messages=include_system_messages,
+                    page_size=page_size,
+                    page_token=page_token,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.ListMessages,
+            request=conversation_interface.ListMessagesRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+                latest_k=latest_k,
+                include_system_messages=include_system_messages,
+                page_size=page_size,
+                page_token=page_token,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def update_message(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        message_uid: str,
+        content: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.UpdateMessageResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.UpdateMessage,
+                request=conversation_interface.UpdateMessageRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                    message_uid=message_uid,
+                    content=content,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.UpdateMessage,
+            request=conversation_interface.UpdateMessageRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+                message_uid=message_uid,
+                content=content,
+            ),
+            metadata=self.hosts[self.instance].metadata,
+        ).send_sync()
+
+    @grpc_handler
+    def delete_message(
+        self,
+        namespace_id: str,
+        catalog_id: str,
+        conversation_id: str,
+        message_uid: str,
+        async_enabled: bool = False,
+    ) -> conversation_interface.DeleteMessageResponse:
+        if async_enabled:
+            return RequestFactory(
+                method=self.hosts[self.instance].async_client.DeleteMessage,
+                request=conversation_interface.DeleteMessageRequest(
+                    namespace_id=namespace_id,
+                    catalog_id=catalog_id,
+                    conversation_id=conversation_id,
+                    message_uid=message_uid,
+                ),
+                metadata=self.hosts[self.instance].metadata,
+            ).send_async()
+
+        return RequestFactory(
+            method=self.hosts[self.instance].client.DeleteMessage,
+            request=conversation_interface.DeleteMessageRequest(
+                namespace_id=namespace_id,
+                catalog_id=catalog_id,
+                conversation_id=conversation_id,
+                message_uid=message_uid,
             ),
             metadata=self.hosts[self.instance].metadata,
         ).send_sync()
