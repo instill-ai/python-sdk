e494382cce26cc29065fdb6f199ddfbb77fde462
Heiru Wu <heiru.wu@instill.tech>
2024-10-07 23:16:16 +0800

    refactor(client): revive indivisual client initialization

--- instill/clients/__init__.py
+++ instill/clients/__init__.py
@@ -1,4 +1,11 @@
-from instill.clients.client import InstillClient, init_core_client
+from instill.clients.client import (
+    InstillClient,
+    init_artifact_client,
+    init_core_client,
+    init_mgmt_client,
+    init_model_client,
+    init_pipeline_client,
+)
 from instill.clients.mgmt import MgmtClient
 from instill.clients.model import ModelClient
 from instill.clients.pipeline import PipelineClient
