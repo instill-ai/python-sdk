a649179d4b89f62ea2d2a2535a3945fa827fc254
george <george.strong@instill.tech>
2025-04-15 16:49:20 +0100

    fix(clients): add secure argument to latest SDK and remove app service

--- instill/clients/client.py
+++ instill/clients/client.py
@@ -1,6 +1,5 @@
 # pylint: disable=no-name-in-module,no-member
 import instill.protogen.core.mgmt.v1beta.mgmt_pb2 as mgmt_interface
-from instill.clients.app import AppClient
 from instill.clients.artifact import ArtifactClient
 from instill.clients.mgmt import MgmtClient
 from instill.clients.model import ModelClient
@@ -67,16 +66,6 @@ class InstillClient:
                 "Instill Artifact is not serving, Artifact functionalities will not work"
             )
 
-        self.app = AppClient(
-            api_token=api_token,
-            url=url,
-            secure=secure,
-            lookup_func=self._lookup_namespace_uid,
-            requester_id=requester_id,
-            async_enabled=async_enabled,
-        )
-        if not self.app.is_serving():
-            Logger.w("Instill App is not serving, App functionalities will not work")
 
     def _lookup_namespace_uid(self, namespace_id: str):
         resp = self.mgmt.check_namespace(namespace_id)
@@ -97,14 +86,12 @@ class InstillClient:
         self.pipeline.close()
         self.model.close()
         self.artifact.close()
-        self.app.close()
 
     async def async_close(self):
         self.mgmt.async_close()
         self.pipeline.async_close()
         self.model.async_close()
         self.artifact.async_close()
-        self.app.async_close()
 
     def get_mgmt(self) -> MgmtClient:
         return self.mgmt
@@ -118,21 +105,20 @@ class InstillClient:
     def get_model(self) -> ModelClient:
         return self.model
 
-    def get_app(self) -> AppClient:
-        return self.app
-
 
 def init_core_client(
     api_token: str,
     requester_id="",
     url: str = HOST_URL_PROD,
     async_enabled: bool = False,
+    secure: bool = True,
 ) -> InstillClient:
     return InstillClient(
         api_token=api_token,
         requester_id=requester_id,
         url=url,
         async_enabled=async_enabled,
+        secure=secure,
     )
 
 
@@ -141,12 +127,14 @@ def init_artifact_client(
     requester_id: str = "",
     url: str = HOST_URL_PROD,
     async_enabled: bool = False,
+    secure: bool = True,
 ) -> ArtifactClient:
     client = InstillClient(
         api_token=api_token,
         requester_id=requester_id,
         url=url,
         async_enabled=async_enabled,
+        secure=secure,
     )
     if not client.get_artifact().is_serving():
         Logger.w(
@@ -162,12 +150,14 @@ def init_model_client(
     requester_id="",
     url: str = HOST_URL_PROD,
     async_enabled: bool = False,
+    secure: bool = True,
 ) -> ModelClient:
     client = InstillClient(
         api_token=api_token,
         requester_id=requester_id,
         url=url,
         async_enabled=async_enabled,
+        secure=secure,
     )
     if not client.get_model().is_serving():
         Logger.w("Instill Model is not serving, Model functionalities will not work")
@@ -181,12 +171,14 @@ def init_pipeline_client(
     requester_id="",
     url: str = HOST_URL_PROD,
     async_enabled: bool = False,
+    secure: bool = True,
 ) -> PipelineClient:
     client = InstillClient(
         api_token=api_token,
         requester_id=requester_id,
         url=url,
         async_enabled=async_enabled,
+        secure=secure,
     )
     if not client.get_pipeline().is_serving():
         Logger.w("Instill VDP is not serving, VDP functionalities will not work")
@@ -200,34 +192,17 @@ def init_mgmt_client(
     requester_id="",
     url: str = HOST_URL_PROD,
     async_enabled: bool = False,
+    secure: bool = True,
 ) -> MgmtClient:
     client = InstillClient(
         api_token=api_token,
         requester_id=requester_id,
         url=url,
         async_enabled=async_enabled,
+        secure=secure,
     )
     if not client.get_mgmt().is_serving():
         Logger.w("Instill Core is required")
         raise NotServingException
 
     return client.get_mgmt()
-
-
-def init_app_client(
-    api_token: str = "",
-    requester_id: str = "",
-    url: str = HOST_URL_PROD,
-    async_enabled: bool = False,
-) -> AppClient:
-    client = InstillClient(
-        api_token=api_token,
-        requester_id=requester_id,
-        url=url,
-        async_enabled=async_enabled,
-    )
-    if not client.get_app().is_serving():
-        Logger.w("Instill App is not serving, App functionalities will not work")
-        raise NotServingException
-
-    return client.get_app()
